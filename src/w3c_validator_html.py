809c639c48073e452ee00ffe6279f381a0aabbba
Joshua Miller <jsmiller@nascar.com>
2013-12-05 10:23:28 -0500

    Added original script for HTML validation

---
+++ src/w3c_validator_html.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+# import HTML validator
+from py_w3c.validators.html.validator import HTMLValidator
+import time, sys
+
+
+# Set the URL to validate
+if len(sys.argv) < 1:
+	url = 'http://www.nascar.com'
+else:
+	url = sys.argv[1]
+	
+	
+# Indicate if the reults should be dumped to the console
+if len(sys.argv) < 3:
+	dump = 0
+else:
+	dump = sys.argv[2]
+	
+# Specify a file path for storing results
+pt = '/Users/jsmiller/Desktop/'
+	
+# Create a filename to store the results in
+fn = 'W3C_Validator_Results_' + time.strftime('%m%d%y_%H%M%S') + '.txt'
+
+
+# Create validator object
+vld = HTMLValidator()
+
+# Validate URL
+vld.validate(url)
+
+# Capture validator errors
+e = vld.errors
+
+# Capture validator warnings
+w = vld.warnings
+
+# Create a line separator
+sep = ''.join(['=']*80)
+
+# Output the results
+res = ''
+res = ''.join([res,sep])
+res = '\n'.join([res,'W3C VALIDATOR RESULTS'])
+res = '\n'.join([res,sep])
+res = '\n'.join([res,'URL:' + url])
+res = '\n'.join([res,'Date/Time:' + time.strftime('%c')])
+res = '\n'.join([res,sep])
+res = '\n'.join([res,'Total Errors:' + str(len(e))])
+res = '\n'.join([res,sep])
+res = '\n'.join([res,'Error Summary:'])
+res = '\n'.join([res,sep])
+
+# Itereate over each error item
+for item in e:
+		res = '\n'.join([res,'Line: ' + item['line'] + ' - ' + item['message']])
+
+
+# Output the results to a new file
+text_file = open(pt+fn, "w")
+text_file.write(str(res))
+text_file.close()
+
+# Respond to the user
+print 'Results written to new file:', fn
+
+
+# Print the results if asked
+if int(dump) == 1:
+	print str(res)
+
+
